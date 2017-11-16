7fd6391ed957822fd26a76b062e4a412cbe048ee
Nevix <dmitriy.korzunov@gmail.com>
2017-11-16 17:51:45 +0200

    html formatting

---
+++ src/utils.js
@@ -0,0 +1,28 @@
+import RateLimit from 'telegraf-ratelimit';
+
+const BOT_ID = process.env.BOT_TOKEN.split(':')[0];
+
+export const isMe = (user) => Number(user.id) === Number(BOT_ID);
+
+export const getUsername = (user, showAt = true) => {
+	if (user.username) return (showAt ? '@' : '') + user.username;
+	if (user.first_name && user.last_name) return `${user.first_name} ${user.last_name}`;
+	if(user.first_name) return user.first_name;
+	if(user.last_name) return user.last_name;
+	return user.id;
+};
+
+export const limiter = new RateLimit({
+	window: 5000,
+	limit: 2,
+	onLimitExceeded: (ctx, next) => ctx.reply('Wait...'),
+});
+
+export const esc = (strings, ...values) => {
+	let str = '';
+	strings.forEach((s, i) => {
+		str += s + (values[i] ? String(values[i]).replace(/(?=[*_`\[])/g, '\\') : '');
+	});
+	console.log(str)
+	return str;
+};
